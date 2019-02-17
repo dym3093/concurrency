1c0ce8999cda6c8bf08a10767b86b4ca228e4f91
dengyouming <dengyouming@nokia.com>
2019-02-17 18:37:35 +0800

    add annoations

---
+++ src/main/java/com/dayton/concurrency/annoation/NotRecommend.java
@@ -0,0 +1,8 @@
+package com.dayton.concurrency.annoation;
+
+/**
+ * @author Martin Deng
+ * @since 2019-02-17 18:33
+ */
+public @interface NotRecommend {
+}
---
+++ src/main/java/com/dayton/concurrency/annoation/ThreadSafe.java
@@ -0,0 +1,8 @@
+package com.dayton.concurrency.annoation;
+
+/**
+ * @author Martin Deng
+ * @since 2019-02-17 18:28
+ */
+public class ThreadSafe {
+}
